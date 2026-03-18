b173243323c1f183fb63bd3fa6310e533946de2b
Noticxs <munchablesofficial@outlook.com>
2026-03-18 07:41:41 +0000

    Updates to search version 2.0.0, main init

---
+++ src/include/utilities.hh
@@ -0,0 +1,52 @@
+// Search | Search for files and directories easier
+// Copyright (C) 2026  Ametrine Foundation
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef UTILITIES_HH
+#define UTILITIES_HH
+
+#include <string>
+
+// Colors for logging
+struct Colors {
+    public:
+    inline static const char *red = "\033[31m";
+    inline static const char *green = "\033[32m";
+    inline static const char *blue = "\033[34m";
+    inline static const char *yellow = "\033[33m";
+    inline static const char *magenta = "\033[35m";
+    inline static const char *cyan = "\033[36m";
+    inline static const char *white = "\033[37m";
+    inline static const char *RESET = "\033[0m";
+};
+
+// Main utilities for search functionality
+struct Utilities {
+    static bool should_log;
+    static bool explore_all;
+    static bool file_search;
+    static bool ignore_gitignore;
+
+    static std::string path;
+
+};
+
+#define MAX_BUFFER_SIZE 1024
+
+// Logging function
+void logs(const char *function, const char *message);
+void elogs(const char *message);
+
+#endif
---
+++ src/main.cc
@@ -0,0 +1,97 @@
+// Search | Search for files and directories easier
+// Copyright (C) 2026  Ametrine Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "include/utilities.hh"
+#include <cstring>
+
+bool Utilities::should_log = false;
+bool Utilities::explore_all = false;
+bool Utilities::file_search = false;
+bool Utilities::ignore_gitignore = false;
+std::string Utilities::path = "";
+
+int main(int argc, char *argv[]) {
+    char buffer[MAX_BUFFER_SIZE];
+
+    logs(__FUNCTION__, "Search for files and directories easier");
+
+    // if debug flags are found enable logging
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--debug") == 0) {
+            Utilities::should_log = true;
+            logs(__FUNCTION__, "Debug mode enabled");
+            continue;
+        }
+
+        if (strcmp(argv[i], "--full") == 0) {
+            if (Utilities::explore_all) {
+                logs(__FUNCTION__, "--full flag ignored, explore_all is already true");
+            } else {
+                logs(__FUNCTION__, "Full exploration enabled");
+                Utilities::explore_all = true;
+            }
+
+            continue;
+        }
+
+        if (strcmp(argv[i], "--file") == 0) {
+            if (Utilities::file_search) {
+                logs(__FUNCTION__, "--file flag ignored, file_search is already true");
+            } else {
+                logs(__FUNCTION__, "File search enabled");
+                Utilities::file_search = true;
+            }
+
+            continue;
+        }
+
+        if (strcmp(argv[i], "--ignore-gitignore") == 0) {
+            if (Utilities::ignore_gitignore) {
+                logs(__FUNCTION__, "--ignore-gitignore flag ignored, already set");
+            } else {
+                logs(__FUNCTION__, "Ignore .gitignore enabled");
+                Utilities::ignore_gitignore = true;
+            }
+
+            logs(__FUNCTION__, "Ignoring .gitignore can cause some issues for maintainers of projects, search takes no responsibility for any issues that may arise from ignoring .gitignore");
+            continue;
+        }
+
+    }
+
+    if (argc < 2) {
+        elogs("Usage: <file name or directory path>");
+        return 1;
+    }
+
+    std::string path = argv[1];
+    Utilities::path = path;
+
+    if (path.length() <= 1) {
+        elogs("path must be a valid directory or file path to search");
+        return 1;
+    }
+
+    if (strcmp(path.c_str(), "/") == 0) {
+        snprintf(buffer, sizeof(buffer), "Root directory: %s", argv[0]);
+        logs(__FUNCTION__, buffer);
+    } else {
+        snprintf(buffer, sizeof(buffer), "File: %s", path.c_str());
+        logs(__FUNCTION__, buffer);
+    }
+
+    return 0;
+}
